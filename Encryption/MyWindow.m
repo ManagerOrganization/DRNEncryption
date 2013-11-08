ccc6587533516ac1187eda3820ef4bf0fbf5921f
Rishabh Tayal <rtayal11@gmail.com>
2013-11-08 10:02:16 -0600

    error fix in reading file data from disk.

--- Encryption/MyWindow.m
+++ Encryption/MyWindow.m
@@ -21,13 +21,17 @@
     NSInteger clicked = [panel runModal];
     
     if (clicked == NSFileHandlingPanelOKButton) {
-        NSData* rawData = [NSData dataWithContentsOfFile:[[panel URLs] objectAtIndex:0]];
+        _label.stringValue = @"Error encrypting the file. Please contact your developer.";
+       NSString* rawDataString = [NSString stringWithContentsOfURL:[[panel URLs] objectAtIndex:0] encoding:NSUTF8StringEncoding error:nil] ;
+        NSData* rawData = [rawDataString dataUsingEncoding:NSUTF8StringEncoding];
         NSData* encryptedData = [Encrypter encryptData:rawData key:ENCRYPTIONKEY initVec:ENCRYPTIONIV];
         NSString* fileName =  (NSString*) [[[[[panel URLs] objectAtIndex:0]absoluteString] lastPathComponent] stringByDeletingPathExtension];
         NSString* filePath = (NSString*) [[[panel URLs] objectAtIndex:0] absoluteString];
         NSString* destinationPath = [filePath stringByReplacingOccurrencesOfString:fileName withString:[NSString stringWithFormat:@"%@-encrypted", fileName]];
-        [encryptedData writeToURL:[NSURL URLWithString:destinationPath] options:NSDataWritingAtomic error:nil];
-        _label.stringValue = @"File encrypted successfully. If you encounter any issues please contact the developer.";
+           BOOL dataWrittingResult = [encryptedData writeToURL:[NSURL URLWithString:destinationPath] options:NSDataWritingAtomic error:nil];
+        if (dataWrittingResult == TRUE) {
+            _label.stringValue = @"File encrypted successfully. If you encounter any issues please contact the developer.";
+        }
     }
 }
 
