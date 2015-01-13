97dc8600c429c2d827749ad6bb49b0adaa1e779c
Rishabh Tayal <rtayal11@gmail.com>
2015-01-13 10:39:17 -0600

    Commit.

--- Encryption/MyWindow.m
+++ Encryption/MyWindow.m
@@ -22,17 +22,13 @@
     
     if (clicked == NSFileHandlingPanelOKButton) {
         _label.stringValue = @"Error encrypting the file. Please contact your developer.";
-//       NSString* rawDataString = [NSString stringWithContentsOfURL:[[panel URLs] objectAtIndex:0] encoding:NSASCIIStringEncoding error:&error] ;
-//        NSData* rawData = [rawDataString dataUsingEncoding:NSASCIIStringEncoding];
         NSData* rawData = [NSData dataWithContentsOfURL:[panel URLs][0]];
-//        NSData* encryptedData = [Encrypter encryptData:rawData key:ENCRYPTIONKEY initVec:ENCRYPTIONIV];
-//        NSData* encryptedData = [Encrypter newEncryptData:rawData key:ENCRYPTIONKEY initVec:ENCRYPTIONIV];
-//        NSData* decryptedData = [Encrypter decryptData:rawData key:ENCRYPTIONKEY initVec:ENCRYPTIONIV];
-        NSData* decryptedData = [Encrypter newDecryptData:rawData key:ENCRYPTIONKEY initVec:ENCRYPTIONIV];
+        NSData* encryptedData = [Encrypter newEncryptData:rawData key:ENCRYPTIONKEY initVec:ENCRYPTIONIV];
+//        NSData* decryptedData = [Encrypter newDecryptData:rawData key:ENCRYPTIONKEY initVec:ENCRYPTIONIV];
         NSString* fileName =  (NSString*) [[[[[panel URLs] objectAtIndex:0]absoluteString] lastPathComponent] stringByDeletingPathExtension];
         NSString* filePath = (NSString*) [[[panel URLs] objectAtIndex:0] absoluteString];
         NSString* destinationPath = [filePath stringByReplacingOccurrencesOfString:fileName withString:[NSString stringWithFormat:@"%@-encrypted", fileName]];
-           BOOL dataWrittingResult = [decryptedData writeToURL:[NSURL URLWithString:destinationPath] options:NSDataWritingAtomic error:nil];
+           BOOL dataWrittingResult = [encryptedData writeToURL:[NSURL URLWithString:destinationPath] options:NSDataWritingAtomic error:nil];
         if (dataWrittingResult == TRUE) {
             _label.stringValue = @"File encrypted successfully. If you encounter any issues please contact the developer.";
         }
